bde73fca3b79706c4af057d90dc398812b4efc9f
ChidiOmen <ochidimon@utexas.edu>
2016-03-26 14:58:38 -0500

    Implement basic controls

--- src/game.js
+++ src/game.js
@@ -4,10 +4,12 @@ Game = {
 		width:  24,
 		height: 16,
 		tile: {
-			width:  16,
-			height: 16
+			width:  32,
+			height: 32
 		}
 	},
+
+	number: 1,
 
 	// The total width of the game screen. Since our grid takes up the entire screen
 	//  this is just the width of a tile times the width of the grid
@@ -24,13 +26,44 @@ Game = {
 	// Initialize and start our game
 	start: function() {
 		// Start crafty and set a background color so that we can see it's working
-		Crafty.init(400, 400);
+		Crafty.init(Game.width(), Game.height());
+		Crafty.background('rgb(249, 223, 125');
+
+		for (var x = 0; x < Game.map_grid.width; x++) {
+			for (var y = 0; y < Game.map_grid.height; y++) {
+				var at_edge = x == 0 || x == Game.map_grid.width - 1 ||
+					y == 0 || y == Game.map_grid.height - 1;
+
+				if (at_edge) {
+					// Place a tree entity at the current tile
+					Crafty.e('Tree').at(x, y);
+				} else if (Math.random() < 0.06) {
+					// Place a bush entity at the current tile
+					Crafty.e('Bush').at(x, y);
+				}
+			}
+		}
 		var square = Crafty.e('2D, Canvas, Color')
-			.attr({x: 10, y: 10, w: 100, h: 100})
+			.attr({x: 0, y: 0, 
+				w: Game.map_grid.tile.width, h: Game.map_grid.tile.height})
 			.color('red')
-			.bind("EnterFrame", function(evenData) {
+			.bind("EnterFrame", function(eventData) {
 				// Move to the right by 10 pixels per second
-				this.x = this.x + 10 * (evenData.dt / 1000);
+				this.x = this.x + Game.number * (eventData.dt / 100);
+			})
+			.bind('KeyDown', function(e) {
+				if (e.key == Crafty.keys.DOWN_ARROW) {
+					this.y += Game.map_grid.tile.height;
+				}
+				if (e.key == Crafty.keys.UP_ARROW) {
+					this.y -= Game.map_grid.tile.height;
+				}
+				if (e.key == Crafty.keys.LEFT_ARROW) {
+					this.x -= Game.map_grid.tile.width;
+				}
+				if (e.key == Crafty.keys.RIGHT_ARROW) {
+					this.x += Game.map_grid.tile.width;
+				}
 			})
 
 	}
