59d036d56286cff96f388fad73d5fe2571e15a10
ChidiOmen <ochidimon@utexas.edu>
2016-03-26 20:24:24 -0500

    Tweaked auto demo level design

--- src/game.js
+++ src/game.js
@@ -36,19 +36,27 @@ Game = {
 				var at_edge = x == 0 || x == Game.map_grid.width - 1 ||
 					y == 0 || y == Game.map_grid.height - 1;
 
-				var taken = (x == 5 && y == 5) || (x == 10 && y == 10);
+				var taken = (x == 5 && y == 5) || (x == 10 && y == 10) || 
+				y == 6 || y == 8 || y == 10;
 
 				if (at_edge) {
 					// Place a tree entity at the current tile
 					Crafty.e('Tree').at(x, y);
-				} else if (Math.random() < 0.06 && !taken) {
+				} else if (Math.random() < 0.3 && !taken) {
 					// Place a bush entity at the current tile
 					Crafty.e('Bush').at(x, y);
 				}
 			}
 		}
 
-		Crafty.e('Pushable').at(10, 10);
+		for (var i = 1; i < 24; i++) {
+			if (i % 4 != 0) {
+				Crafty.e('Pushable').at(i, 8);
+			}
+		}
+		for (var i = 0; i < 6; i++) {
+			Crafty.e('Pushable').at(i*4, 6+4*Crafty.math.randomInt(0,1));
+		}
 		Crafty.e('PlayerCharacter, henry').at(5,5);
 
 	}
